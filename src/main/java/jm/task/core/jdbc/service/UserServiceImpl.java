f388891e493543c21fdada3a5175d0691c7baf51
DarkStayer <DarkStayer@yandex.ru>
2021-01-23 21:31:09 +0800

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -7,197 +8,31 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserServiceImpl extends Util implements UserService {
+public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
 
 
     public void createUsersTable() {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = null;
-        String sql = "CREATE TABLE `users`.`users` (\n" +
-                "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                "  `name` VARCHAR(45) NOT NULL,\n" +
-                "  `lastName` VARCHAR(45) NOT NULL,\n" +
-                "  `age` INT NOT NULL,\n" +
-                "  PRIMARY KEY (`id`))";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
-            System.out.println("Таблица users добавлена");
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
+        super.createUsersTable();
     }
 
     public void dropUsersTable() {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = null;
-        String sql = "DROP TABLE IF EXISTS users;";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
-            System.out.println("Таблица users удалена, либо ее не было");
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
+        super.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = null;
-        String sql = "INSERT INTO users (name, lastName, age) VALUES (?,?,?);";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем- " + name + " добавлен в базу данных");
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
+        super.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = null;
-        String sql = "DELETE FROM users WHERE id=?;";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-            System.out.println("Пользователь с ID = "+ id + " удален");
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
-
+        super.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        Connection connection = getConnection();
-        List<User> userList = new ArrayList<>();
-        String sql = "SELECT id, name, lastName, age FROM users;";
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
-            while (result.next()) {
-                User user = new User();
-                user.setId(result.getLong("id"));
-                user.setName(result.getString("name"));
-                user.setLastName(result.getString("lastName"));
-                user.setAge(result.getByte("age"));
-                userList.add(user);
-            }
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        } finally {
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
-        System.out.println("Пользователи добавлены в коллекцию");
-        return userList;
-
+        return super.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement = null;
-        String sql = "TRUNCATE TABLE users;";
-        try {
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
-            System.out.println("Таблица users очищена");
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-        }finally {
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException sqlException) {
-                    sqlException.printStackTrace();
-                }
-            }
-        }
+        super.cleanUsersTable();
 
     }
-
 }
