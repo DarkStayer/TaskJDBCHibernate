6fdc2fcf4b00c8b3997062c7a316fa04cf31807e
DarkStayer <DarkStayer@yandex.ru>
2021-01-23 18:38:45 +0800

    commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users" + "?autoReconnect=true";
+    private static final String USER = "root";
+    private static final String PASSWORD ="root";
+
+    public Connection getConnection (){
+        Connection connection = null;
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("ERROR");
+        } return null;
+    }
 }
